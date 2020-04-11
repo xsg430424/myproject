db5afffdf099cdbd5bd739852feacdcd6c01aed8
xiaoshuigeng <742748444@qq.com>
2020-04-11 14:00:07 +0800

    删除多余文件

--- myproject/common/src/main/java/com/jinjue/common/exception/GlobalExceptionHandlerAdvice.java
+++
@@ -1,8 +0,0 @@
-package com.jinjue.demo.exception;
-
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class GlobalExceptionHandlerAdvice extends DefaultGlobalExceptionHandlerAdvice {
-
-}
